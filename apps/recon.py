e15943bc3a1a7b05628c213023e3c9bd40fdb434
R. Lugsami <88761252+RONLUG@users.noreply.github.com>
2024-08-04 14:04:12 +0200

    Delete apps/recon.py

--- apps/recon.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('-r', '--resolution', type=int, default=256)
-
-args = parser.parse_args()
-
-subprocess.run(['python', '-m', 'apps.rect_gen', '-r', '256', '--use_rect', '-i', image_path])
